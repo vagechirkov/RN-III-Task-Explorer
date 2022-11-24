f8897cee3cc9335fb6f2a82e3a8c966e19e988f5
Bonati <sarabonati96@gmail.com>
2022-11-24 11:54:36 +0100

    add initial input widgets

--- app/pages/1_generate.py
+++ app/pages/1_generate.py
@@ -1,11 +1,55 @@
 import streamlit as st
 import time
 import numpy as np
+from generate import Network_Generator
+from solve import Rule_Agent
 
-st.set_page_config(page_title="Generate a Network", page_icon="📈")
-
+st.set_page_config(page_title="Generate a Network")
 st.markdown("# Generate a Network")
 st.sidebar.header("Generate")
-st.write(
-    """TODO"""
-)
+
+# submit parameters for generation
+params = {}
+with st.form("generate_form",clear_on_submit = False):
+
+    st.write("Select the generation parameters")
+    # how many networks to generate?
+    params['n_networks'] = st.number_input(label='How many networks do you want to generate?',
+                                          min_value=1,
+                                          max_value=100000,
+                                          value=1,
+                                          step=10)
+    # how many rewards do you want?
+    params['n_rewards'] = st.number_input(label='How many rewards in the network?',
+                                          min_value=2,
+                                          max_value=5,
+                                          value=5,
+                                          step=1)
+    # what are the reward values?
+    rewards_str = st.text_input(label="Insert the reward values separated by a space",
+                                value="-20 0 20")
+    params['rewards'] = [int(i) for i in rewards_str.split(" ")]
+
+    # how many nodes in each level?
+    #TODO
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Generate")
+    if submitted:
+        st.info('Parameters submitted!')
+        
+        # Network_Generator class
+        G = Network_Generator(params)
+        save_path = "TODO"
+        network = G.generate(save_path)
+        with st.expander("See network in JSON format:"):
+            st.json(network)
+
+        # collect the network solution for differen strategies
+        Myopic_agent = Rule_Agent(network,"myopic")
+        Myopic_agent.solve()
+
+        Loss_agent = Rule_Agent(network,"take_first_loss")
+        Loss_agent.solve()
+   
+
--- app/pages/2_visualize.py
+++ app/pages/2_visualize.py
@@ -2,10 +2,22 @@ import streamlit as st
 import time
 import numpy as np
 
-st.set_page_config(page_title="Visualize a Network and Try to Solve", page_icon="📈")
+st.set_page_config(page_title="Visualize a Network and Try to Solve")
 
 st.markdown("# Visualize a Network and Try to Solve")
 st.sidebar.header("Visualize")
-st.write(
-    """TODO"""
-)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    option = st.selectbox("Which network to visualize?",
+                         ("Email", "Home phone", "Mobile phone")) #TODO get list of network ids
+    
+
+with col2:
+    options = st.multiselect('Which strategy solution do you want to see?',
+                             ['Myopic', 'Loss'],
+                             ['Myopic'])
+
+st.write("Insert custom visualization component here!")
+    
