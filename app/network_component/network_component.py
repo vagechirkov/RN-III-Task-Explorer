923815a0bb4ce35e6799c54dd4f721341463711a
vagechirkov <vagechirkov@gmail.com>
2022-12-07 11:33:39 +0100

    fix starting node in the network

--- app/network_component/network_component.py
+++ app/network_component/network_component.py
@@ -2,7 +2,7 @@ import json
 
 import streamlit.components.v1 as components
 
-BASE_URL = "https://631063bad8ec25de99f6946b-tygnjygkgi.chromatic.com/" \
+BASE_URL = "https://631063bad8ec25de99f6946b-wordcsbsge.chromatic.com/" \
            "iframe.html?"
 STORY_COMPONENT = "id=utils-taskexplorer2--default&viewMode=story"
 
